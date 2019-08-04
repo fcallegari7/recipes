801cb6dcda99bc7186f7c7d6c03ca00b77ae3271
Fabio Callegari <guyit@github.com>
2019-08-03 21:50:44 -0700

    add logout method

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -30,7 +30,9 @@ export class RecipeListComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    this.subscription.unsubscribe();
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
