3debd5dac726e2336265f9f863705aad76e2d6f6
Fabio Callegari <fabiocallegari.it@gmail.com>
2019-08-20 21:32:09 -0700

    update project

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  firebaseAPIKey: 'AIzaSyALNwJrTA6k-GH6GwGhsi4viiqbUIlkyYA',
-  firebaseUrl: 'https://recipebook-1a081.firebaseio.com/recipes.json'
-};
